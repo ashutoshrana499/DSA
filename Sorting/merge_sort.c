3a48397d96e2bcee4f102009ed3c4778629b1c6d
Pramod Shenoy <PramodShenoy@users.noreply.github.com>
2017-05-31 06:35:05 +0000

    stack smashing lol

---
+++ Sorting/merge_sort.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+//function to print the array
+void printArray(int arr[], int n)
+{
+   int i;
+   for (i=0; i < n; i++)
+       printf("%d ", arr[i]);
+   printf("\n");
+}
+// Merges two subarrays of arr[].
+// First subarray is arr[l..m]
+// Second subarray is arr[m+1..r]
+void merge(int arr[], int l, int m, int r)
+{
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 =  r - m;
+ 
+   	//creating the temporary arrays
+   	/* in other implementations,these subarrays may be passed directly to the function
+   	the prototype of such an implementation would have 3 arrays and 3 integer*/
+    int left[n1], right[n2];
+
+    /* Copy data to temp arrays L[] and R[] */
+    for (i = 0; i < n1; i++)
+        left[i] = arr[l + i];
+    for (j = 0; j < n2; j++)
+        right[j] = arr[m + 1+ j];
+ 
+    /* Merge the temp arrays back into arr[l..r]*/
+    i = 0; // Initial index of first subarray
+    j = 0; // Initial index of second subarray
+    k = l; // Initial index of merged subarray
+    while (i < n1 && j < n2)
+    {
+        if (left[i] <= right[j])
+        {
+            arr[k] = left[i];
+            i++;
+        }
+        else
+        {
+            arr[k] = right[j];
+            j++;
+        }
+        k++;
+    }
+ 
+    /* Copy the remaining elements of L[], if there
+       are any */
+    while (i < n1)
+    {
+        arr[k] = left[i];
+        i++;
+        k++;
+    }
+ 
+    /* Copy the remaining elements of R[], if there
+       are any */
+    while (j < n2)
+    {
+        arr[k] = right[j];
+        j++;
+        k++;
+    }
+}
+ 
+/* l is for left index and r is right index of the
+   sub-array of arr to be sorted */
+void mergeSort(int arr[], int l, int r)
+{
+    if (l < r)
+    {
+        // Same as (l+r)/2, but avoids overflow for
+        // large l and h
+        int m = l+(r-l)/2;
+ 
+        // Sort first and second halves
+        mergeSort(arr, l, m);
+        mergeSort(arr, m+1, r);
+ 
+        merge(arr, l, m, r);
+    }
+}
+
+int main()
+{
+	int arr[]={10,3,5,20,24,9};
+	int n=sizeof(arr)/sizeof(int);
+	int i,j;
+	mergeSort(arr,0,n);
+	printArray(arr,n);
+	return 0;
+}
