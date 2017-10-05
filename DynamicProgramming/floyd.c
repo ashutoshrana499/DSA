edb755474256bf79349aee7762cee52af212e8af
Pramod Shenoy <PramodShenoy@users.noreply.github.com>
2017-10-05 17:33:24 +0000

    Create floyd.c

---
+++ DynamicProgramming/floyd.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+int min(int a,int b)
+{
+    if(a>b)
+        return b;
+    else
+        return a;
+}
+#define INF 100000
+int main(int argc, char const *argv[])
+{
+    int n,i,j,k;
+    scanf("%d",&n);
+    int adj[n+1][n+1];
+    for(i=1;i<=n;i++)
+        for(j=1;j<=n;j++)
+            scanf("%d",&adj[i][j]);
+    int r[n+1][n+1];
+    memset(r,0,sizeof(r));
+    for(k=1;k<=n;k++)
+    {
+        for(i=1;i<=n;i++)
+        {
+            for(j=1;j<=n;j++)
+            {
+                adj[i][j] = min(adj[i][j],(adj[i][k]+adj[k][j]));
+            }
+        }
+    }
+    printf("\n");
+    for(i=1;i<=n;i++)
+        {
+            for(j=1;j<=n;j++)
+                printf("%d ",adj[i][j]);
+            printf("\n");
+        }
+    return 0;
+}
